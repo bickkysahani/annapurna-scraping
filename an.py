5ddb57f749253384ec3f186dc0f2c725adc7ba94
bickkysahani <bickkysahani@gmail.com>
2022-05-11 15:05:21 +0545

    update an script

--- an.py
+++ an.py
@@ -5,8 +5,31 @@ import json
 def annapurna_scrape(search_term):
     page_number = 1
     listItems = []
+    articles_count = 0
     while True:
         print("current page : ",page_number)
+        try: 
+            with open('response3.json', 'r') as f:
+                listItems = json.load(f)
+                for item in listItems:
+                 #print(item)
+                 for k,v in item.items():
+                     if k == 'page_number':
+                         #print(k)
+                         if item[k] == page_number:
+                            print("page number ", page_number , "data already exists")
+                            #print(page_number)
+                            page_number += 1
+                            print("current page : ",page_number)
+                            break
+                    
+                     else:
+                        pass
+                    
+        except:
+            print("No file found")
+            print("Creating new file")
+        
         url = f"https://bg.annapurnapost.com/api/search?title={search_term}&page={page_number}"
         try:
             response = requests.get(url)
@@ -16,31 +39,35 @@ def annapurna_scrape(search_term):
         if response.status_code == 200:
             res = json.loads(response.text)
         else:
-            #raise SystemExit(f"Error: {response.status_code}")
-            print("error in page number : ",page_number)
-            page_number += 1
-            continue
-        #res = json.loads(response.text)
+            raise SystemExit(f"Error: {response.status_code}")
 
+        totalPage = res['data']['totalPage']
         total_articles = res['data']['total']
+        articles_count += res['data']['count']
         print("total articles : ",res['data']['total'])
         print("articles count in current page:", res['data']['count'])
         # print(type(res))
-        with open('response2.json', 'w') as f:
-            listItems.extend(res['data']['items'])
-            json.dump(listItems, f, indent=4, ensure_ascii=False,
-                            separators=(',', ': '))
-    
-        print("Total articles saved :",len(listItems))   
+     
+        #listItems.append({'page_number': page_number, 'data': res['data']['items']})
+        dataItems = {'page_number': page_number, 'data': res['data']['items']}
+        listItems.append(dataItems)
 
-        if len(listItems) == total_articles:
-            print("No more pages")
-            break
+        with open('response3.json', 'w') as f:
+            json.dump(listItems, f, indent=4, ensure_ascii=False,
+                             separators=(',', ': '))
+       
+        print("Total articles saved :", articles_count)   
+        print("----------------------------")
+        
 
         page_number += 1
-  
+
+        if page_number > totalPage:
+            print("No more pages")
+            break
         
+  
+       
 
 term = 'शेरबहादुर देउवा'
 annapurna_scrape(term)
-
